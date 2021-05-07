5edcd08e171756aa0962ba749283aea0fe6dc497
Drathijin <rsulbara@ucm.es>
2021-05-07 18:57:13 +0200

    Linux updated

--- build.sh
+++ build.sh
@@ -31,8 +31,10 @@ printmsg() {
 
 #Moves assets to corresponding folders in bin
 move_assets() {
-	cp "-r" "./TaiFighter/assets/." "./bin/assets/"
-	cp "-r" "./TaiFighter/scripts/." "./bin/assets/scripts/Client"
+	# mkdir "./TaiFighter/assets/scripts/Engine"
+	cp -rT "./bin" "./TaiFighter"
+	cp "-r" "./bin/assets/scripts/Engine" "./TaiFighter/assets/scripts/Engine"
+	rm -r ./bin
 }
 
 #Gets the path to the engine by param, builds it and moves assets and scritps to its corresponding places
@@ -83,7 +85,7 @@ build $path_to_engine
 
 #If execute flag is set, execute the game after build
 if [[ ! -z $xflag ]]; then
-	cd bin
+	cd TaiFighter
 	./PTSD_Core
 fi
 exit 0

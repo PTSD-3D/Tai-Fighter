c8ab7e5a556bdbc5d20552520bdd435fa61829f2
Miguel Mur Cortés <mmur@ucm.es>
2021-05-11 17:54:26 +0200

    Comentario en la escena

--- TaiFighter/assets/scripts/Client/sampleScene.lua
+++ TaiFighter/assets/scripts/Client/sampleScene.lua
@@ -19,8 +19,27 @@ local Scene = {
 					scale = {x = 1.0, y = 4.6, z = 1}
 				}
 			}
-		)
+		),
+
+		
+		--The scale and positioning of the models is all messed up (bullets were coming out of their right side)
+		--This is currently being worked on in another branch, so we see it fit to just comment it
+		--so you can all appretiate the god tier rotation animation on this bad boy.
+
+		-- Enemy_001=prefabs.Enemy({
+		-- 	Transform = {position={x=20.0,y=-7.999999046325684,z=-300.0},rotation={x=-90.0,y=720.0,z=-0.0},scale={x=3.0,y=3.0,z=3.0}}}),
+		-- Enemy_002=prefabs.Enemy({
+		-- 	Transform = {position={x=-30.0,y=30.0,z=-190.0},rotation={x=-90.0,y=720.0,z=-0.0},scale={x=3.0,y=3.0,z=3.0}}}),
+		-- Enemy_003=prefabs.Enemy({
+		-- 	Transform = {position={x=30.0,y=30.0,z=-190.0},rotation={x=-90.0,y=720.0,z=-0.0},scale={x=3.0,y=3.0,z=3.0}}}),
+		-- Enemy_004=prefabs.Enemy({
+		-- 	Transform = {position={x=50.0,y=0.0,z=-120.0},rotation={x=-90.0,y=720.0,z=-0.0},scale={x=3.0,y=3.0,z=3.0}}}),
+		-- Enemy_005=prefabs.Enemy({
+		-- 	Transform = {position={x=-50.0,y=0.0,z=-120.0},rotation={x=-90.0,y=720.0,z=-0.0},scale={x=3.0,y=3.0,z=3.0}}}),
+		-- Enemy_006=prefabs.Enemy({
+		-- 	Transform = {position={x=-0.0,y=0.0,z=-150.0},rotation={x=-90.0,y=720.0,z=-0.0},scale={x=3.0,y=3.0,z=3.0}}})
 	},
+
 	SceneConfig = prefabs.sampleScene({})
 }
 return Scene

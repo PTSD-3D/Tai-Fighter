34e7c8a3f61b04301dcd1b71c0cd96dec0cbcf58
AdriAlvarez00 <adralv01@ucm.es>
2021-05-17 18:08:28 +0200

    Logica de colliders, dependencia del motor

--- TaiFighter/assets/scripts/Client/SystemsList.lua
+++ TaiFighter/assets/scripts/Client/SystemsList.lua
@@ -301,8 +301,16 @@ local HealthSystem = ns.class("HealthSystem",ns.System)
 
 function HealthSystem:requires() return {"health"} end
 
-function HealthSystem:changeCollider(isSideView)
-	LOG("Health persp callback")
+function HealthSystem:changeCollider(event)
+	local rbScale = vec3:new(0, 0, 0)
+	if (event.sideview) then
+		LOG("Health collider sideview", LogLevel.Trace,1)
+		rbScale = vec3:new(10000,1,1)
+	else
+		LOG("Health collider 3D", LogLevel.Trace,1)
+		rbScale = vec3:new(1,1,1)
+	end
+	self.player.Rigidbody:setCollisionScale(rbScale)
 end
 
 function HealthSystem:onCollision(player, other, _)

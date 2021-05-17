7f83c3c1bbfd2016e4226851c81423c045de4919
Esteban <35503563+EstebenR@users.noreply.github.com>
2021-05-18 00:12:13 +0200

    Mergeado con main

--- TaiFighter/assets/scripts/Client/Systems/EnemyCollisionSystem.lua
+++ TaiFighter/assets/scripts/Client/Systems/EnemyCollisionSystem.lua
@@ -5,12 +5,8 @@ local EnemyCollisionSystem = ns.class("EnemyCollisionSystem",ns.System)
 function EnemyCollisionSystem:requires() return {"enemyCollision"} end
 
 function EnemyCollisionSystem:onCollision(enemy, other, collision)
-	if(other:has("playerMove")) then
-		LOG("Player DEAD")
-	else
-		LOG("Enemy DEAD")
-		Manager:removeEntity(enemy)
-		end
+	LOG("Enemy DEAD")
+	Manager:removeEntity(enemy)
 end
 
 function EnemyCollisionSystem:update(dt)

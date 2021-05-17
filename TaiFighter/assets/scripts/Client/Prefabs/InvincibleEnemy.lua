e3a34849f2afa92802dda0f081b1be31745de07a
Miguel Mur Cortés <mmur@ucm.es>
2021-05-17 12:49:20 +0200

    Prefab creado

---
+++ TaiFighter/assets/scripts/Client/Prefabs/InvincibleEnemy.lua
@@ -0,0 +1,26 @@
+local pf = require("Prefab")
+
+local function scale(table, factor)
+	return {
+		x = table.x * factor,
+		y = table.y * factor,
+		z = table.z * factor
+	}
+end
+
+-- Params holds required parameters to build an instance of this prefab. This will be populated with transform information from Blender via our script in /tools
+function pf.InvincibleEnemy(params)
+	return {
+		Components = {
+			-- Example user defined components
+			{ name = "enemyMove", arguments = {0.3}},
+			-- { name = "playerMove", arguments = { { x = 1, y = 0, z = 0 } } }
+		},
+		Transform = params.Transform,
+		Mesh = { mesh = "InvincibleEnemy.mesh", material = "InvincibleEnemy" },
+		Rigidbody = {	size=scale(params.Transform.scale, params.Scale or 1.1),
+									mass=1,position=params.Transform.position,
+									type=2,trigger=false,
+									rotation=params.Transform.rotation}
+	}
+end

5dacb01e188ce4a1fc143274031d5b9635b34667
AdriAlvarez00 <adralv01@ucm.es>
2021-05-18 13:04:12 +0200

    Balas y cambio de perspectiva

--- TaiFighter/assets/scripts/Client/Systems/VariableColliderSystem.lua
+++ TaiFighter/assets/scripts/Client/Systems/VariableColliderSystem.lua
@@ -5,25 +5,26 @@ local VariableColliderSystem = ns.class("VariableColliderSystem",ns.System)
 function VariableColliderSystem:requires() return {"variableCollider"} end
 
 function VariableColliderSystem:changeColliderEv(event)
-	local rbScale = vec3:new(0, 0, 0)
 	if (event.sideview) then
-		LOG("Health collider sideview", LogLevel.Trace,1)
-		rbScale = vec3:new(10000,1,1)
+		--LOG("VCol sideview", LogLevel.Trace,1)
+		self.colScale = vec3:new(10000,1,1)
 	else
-		LOG("Health collider 3D", LogLevel.Trace,1)
-		rbScale = vec3:new(1,1,1)
+		--LOG("Vcol collider 3D", LogLevel.Trace,1)
+		self.colScale = vec3:new(1,1,1)
 	end
     for _,entity in pairs(self.targets) do
-	    entity.Rigidbody:setCollisionScale(rbScale)
+	    entity.Rigidbody:setCollisionScale(self.colScale)
     end 
 end
 
 function VariableColliderSystem:onAddEntity(entity)
-    LOG("Hay algo en addEntity")
+    --LOG("[VCOL] entidad anadida", LogLevel.Critical, 1)
+    entity.Rigidbody:setCollisionScale(self.colScale)
 end
 
 function VariableColliderSystem:initialize()
 	ns.System.initialize(self)
+    self.colScale = vec3:new(10000,1,1)
 	Manager.eventManager:addListener("changePerspectiveEvent", self, self.changeColliderEv)
 end
 
